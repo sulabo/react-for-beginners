33cf5562cc98b734116326623e25ee6fe1a1a1c0
sulabo <msw0922@gmail.com>
2022-07-20 14:51:30 +0900

    비트코인 인풋을 통해 현재의 돈으로 살 수 있는 가격을 나타내는 기능 추가

--- src/App.js
+++ src/App.js
@@ -3,7 +3,11 @@ import {useState, useEffect} from 'react';
 
 function App() {
   const [loading, setLoading] = useState(true);
+  const [myMoney, setMyMoney] =useState("");
+  const [coinPrice, setCoinPrice] = useState(Infinity);
+  const [resultMoney, setResultMoney] = useState(0);
   const [coins, setCoins] = useState([]);
+  const [symbols, setSymbols] = useState("");
     useEffect(() => {
     fetch("https://api.coinpaprika.com/v1/tickers")
     .then(response => response.json( ))
@@ -11,17 +15,40 @@ function App() {
     setCoins(json));
     setLoading(false);
   }, [])
+
+  useEffect(() => {
+    setResultMoney(myMoney/coinPrice);
+  }, [myMoney, coinPrice]);
+
+  const onChange = (event) => setMyMoney(event.target.value);
+
+  const onChangeSelect = (event) => {
+    const price = coins[event.target.selectedIndex].quotes.USD.price;
+    const symbol = coins[event.target.selectedIndex].symbol;
+    setCoinPrice(parseFloat(price));
+    setSymbols(symbol);
+  };
+
+
   return (
     <div>
-      <h1>The Coins ({coins.length}) </h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <select>
+      <h1>The Coins {loading ? "" : `(${coins.length})`} </h1>
+      <form >
+      <input value={myMoney} type="number" placeholder="how much money do you have" onChange={onChange} />
+
+      </form>
+      <hr/>
+      {loading ? (<strong>Loading...</strong>) : (<select onChange={onChangeSelect}>
         {coins.map((coin) => (
-        <option>
+        <option key={coin.id}>
           {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
           </option>
           ))}
       </select>
+      )}
+      <h4>
+        your money : ${myMoney} === {resultMoney} {symbols}
+      </h4>
     </div>
   );
 }
