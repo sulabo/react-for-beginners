ef07675cfe7287befb7ab21a9e63a1606e1d052f
sulabo <msw0922@gmail.com>
2022-07-20 13:55:14 +0900

    암호화폐의 현황 가져오기

--- src/App.js
+++ src/App.js
@@ -2,34 +2,26 @@ import {useState, useEffect} from 'react';
 
 
 function App() {
-  const [toDo, setToDo] = useState("");
-  const [toDos, setToDos] =useState([]);
-  const onChange = (event) => setToDo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault() ;
-    if(toDo === ""){
-      return;
-    }
-    setToDos(currentArray => [toDo, ...currentArray]); 
-    setToDo("");
-  };
-  console.log(toDos);
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+    useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+    .then(response => response.json( ))
+    .then((json) => 
+    setCoins(json));
+    setLoading(false);
+  }, [])
   return (
     <div>
-      <h1>My To Do ({toDos.length}) </h1>
-      <form onSubmit={onSubmit}>
-      <input 
-      onChange={onChange} 
-      value={toDo} 
-      type="text" 
-      placeholder="write your to do" 
-      />
-      <button >Add To Do</button>
-      </form>
-      <hr />
-      <ul>
-      {toDos.map((item, index) => <li key={index}>{item}</li>)}
-      </ul>
+      <h1>The Coins ({coins.length}) </h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select>
+        {coins.map((coin) => (
+        <option>
+          {coin.name} ({coin.symbol}) : ${coin.quotes.USD.price} USD
+          </option>
+          ))}
+      </select>
     </div>
   );
 }
